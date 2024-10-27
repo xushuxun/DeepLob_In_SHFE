c78bd78ed1aa4b784df393d2e86aacbba01ebd42
xushuxun <xushuxun@outlook.com>
2024-10-28 02:13:41 +0800

    refactor

--- download.py
+++ download.py
@@ -4,13 +4,24 @@ from contextlib import closing
 from tqsdk import TqApi, TqAuth, TqSim
 from tqsdk.tools import DataDownloader
 
-# 输入你的天勤tqsdk账号密码
-api = TqApi(auth=TqAuth("", ""))
-download_tasks = {}
+import os
+api = TqApi(auth=TqAuth(os.environ['tqname'], os.environ['tqpassword']))
 
-download_tasks["T_tick"] = DataDownloader(api, symbol_list=["SHFE.rb2501"], dur_sec=0,
-                    start_dt=datetime(2024, 7, 1), end_dt=datetime(2024, 9, 10), csv_file_name="rb2501_tick.csv")
-# 使用with closing机制确保下载完成后释放对应的资源
+exchange = "INE"
+future = "nr2412"
+symbol = f"{exchange}.{future}"
+start_date = datetime(2024, 9, 26)
+end_date = datetime(2024, 10, 24)
+
+
+
+download_tasks = {}
+download_tasks[future] = DataDownloader(api, symbol_list=[symbol], 
+                                        dur_sec=0,
+                                        start_dt=start_date, 
+                                        end_dt=end_date, 
+                                        csv_file_name=f'{future}.csv')
+
 with closing(api):
     while not all([v.is_finished() for v in download_tasks.values()]):
         api.wait_update()
